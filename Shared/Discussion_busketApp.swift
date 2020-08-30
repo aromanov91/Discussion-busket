cb3984b44517dbddafeb8c658190a8e87dee2b41
Alexander Romanov <aromanov07@gmail.ru>
2020-08-30 21:43:51 +0300

    Initial Commit

---
+++ Shared/Discussion_busketApp.swift
@@ -0,0 +1,17 @@
+//
+//  Discussion_busketApp.swift
+//  Shared
+//
+//  Created by 18391981 on 30.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct Discussion_busketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
